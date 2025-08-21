4dd1b11a561ff9b9f4b2af341498504bc64cbd83
Matt McKenna <matthew.w.mckenna@spiritaero.com>
2025-08-21 11:00:59 -0500

    Batch file fixes and file structure placeholders

--- build.bat
+++ build.bat
@@ -3,14 +3,14 @@
 mkdir build
 pushd build
 cl -Zi -DWIN32 -D_WIN32 -DNOGDI -DNOUSER -O3 ^
--Iinclude ^
--I..lib\raylib\winx64_msvc16\include ^
--I..lib\xiAPI\include ^
+-I..\include ^
+-I..\lib\raylib\include ^
+-I..\lib\xiAPI\include ^
 ..\src\main.c ^
 user32.lib winmm.lib gdi32.lib msvcrt.lib shell32.lib ^
 raylib.lib xiapi64.lib ^
--link -libpath:..lib\raylib\winx64_msvc16 ^
--libpath:..lib\xiAPI ^
+-link -libpath:..\lib\raylib\winx64_msvc16 ^
+-libpath:..\lib\xiAPI ^
 -NODEFAULTLIB:libcmt ^
 -out:xiclops.exe
 popd
